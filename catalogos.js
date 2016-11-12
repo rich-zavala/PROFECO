23fb373637d1291d86af9467de1c8ce8446f376a
Ricardo <rich.zavalac@gmail.com>
2016-11-12 13:42:10 -0600

    Iconos y esquema inicial

---
+++ catalogos.js
@@ -0,0 +1,36 @@
+var supermercados = [
+	{ id: 0, name: "Walmart"  },
+	{ id: 1, name: "Chedrauii"  },
+	{ id: 2, name: "Superama"  },
+	{ id: 3, name: "Aurrera"  },
+	{ id: 4, name: "Aki"  },
+	{ id: 5, name: "Elektra"  },
+	{ id: 6, name: "Amazon"  },
+	{ id: 7, name: "Dunosusa"  },
+	{ id: 8, name: "Comercial Mexicana"  }
+];
+
+var catalogo = [
+	{ id: 0, name: "Jabón", precio: 10 },
+	{ id: 1, name: "Huevo", precio: 16 },
+	{ id: 2, name: "Leche", precio: 16 },
+	{ id: 3, name: "Frijol", precio: 14 },
+	{ id: 4, name: "Arroz", precio: 20 },
+	{ id: 5, name: "Detergente", precio: 10 },
+	{ id: 6, name: "Aceite", precio: 10 },
+	{ id: 7, name: "Shampoo", precio: 10 },
+	{ id: 8, name: "Jamón", precio: 10 },
+	{ id: 9, name: "Queso", precio: 10 },
+	{ id: 10, name: "Pan", precio: 10 },
+	{ id: 11, name: "Televisión 50 pulgadas", precio: 10 },
+	{ id: 12, name: "Xbox One", precio: 10 },
+	{ id: 13, name: "Celular Galaxia 35", precio: 10 },
+	{ id: 14, name: "Computadora De Él", precio: 10 }
+];
+
+var precios = [];
+for(var is in supermercados){
+	for(var ic in catalogo){
+		
+	}
+}
